b4c3e8f45bffe8fa151e00d3b7e7473ad1b446c3
marcosmarinho <139090070+marcosfrancomarinho@users.noreply.github.com>
2026-08-02 23:09:08 -0300

    Atualizar o links.ts

--- src/data/links.ts
+++ src/data/links.ts
@@ -33,7 +33,7 @@ export const links: LinkItem[] = [
   },
   {
     title: 'Spotify',
-    description: 'A trilha sonora para as segundas-feiras',
+    description: 'A melhores músicas catalogadas',
     href: 'https://open.spotify.com/playlist/47f2vCrvOn7hSKpaMMz2vw?si=SBx9yelwR4uBvJHSt5gq-g&utm_source=whatsapp&pi=UQBnD7GuQ7SIc',
     icon: FaSpotify,
     color: 'green',
